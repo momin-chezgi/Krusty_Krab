419f3e1bfc41b8c0ffd70e449c7b0c114ebe1e3b
momin-chezgi <momin.chezgi@gmail.com>
2026-06-07 14:50:33 +0330

    Raw enumerations turn to enum class

--- include/Domain/Enums.h
+++ include/Domain/Enums.h
@@ -1,51 +1,73 @@
 #pragma once
 # include "Common/Types.h"
 
+enum class Role : int {
+    Quit = 0,
+    Customer = 1,
+    Restaurateur = 2,
+    SystemAdmin = 3
+};
 
-enum class Role { Customer, RestaurantManager, SystemAdmin };
+enum class RestaurantStatus : int {
+    Inactive = 0,
+    Active = 1
+};
 
-enum class RestaurantStatus { Active, Inactive };
+enum class ItemType : int {
+    Food = 1,
+    Drink = 2
+};
 
-enum class ItemType { Food, Drink };
-
-
-enum OrderStatus{
+enum class OrderStatus : int {
     InPreparation,
     ReadyToSend,
     Delivered,
     Cancelled
 };
 
-enum CustomerOpt{
-    OrderOut,
-    EditOrder,
-    RemoveOrder,
-
-    Quit,
+enum class CustomerAction : int {
+    Quit = 0,
+    PlaceOrder = 1,
+    ViewMyOrders = 2,
+    DebugStorage = 404
 };
 
-enum ManagerOpt{
-    EditName,
-    EditLinkedRest,
+enum class RestaurateurAction : int {
+    Quit = 0,
     // restaurant:
-    EditRestName,
-    EditAddress,
-    Activate,
-    Deactivate,
-    EditMinutes,
-    EditPhone,
-    EditBio,
+    EditRestaurantName = 1,
+    EditRestaurantAddress = 2,
+    ActivateRestaurant = 3,
+    DeactivateRestaurant = 4,
+    EditRestaurantMinutes = 5,
+    EditRestaurantPhone = 6,
+    EditRestaurantBio = 7,
     // menu:
-    AddItem2Menu,
-    DelItemFromMenu,
-    ReplaceItemInMenu,
+    AddItemToMenu = 11,
+    RemoveItemFromMenu = 12,
+    ReplaceItemInMenu = 13,
+    // queue:
+    AddOrderToQueue = 21,
+    RemoveOrderFromQueue = 22,
+    ReplaceOrderInQueue = 23,
+    // statistics:
+    PrintSaleStatistics = 31,
+    PrintCustomerStatistics = 32,
+    // own profile:
+    EditRestaurateurName = 41,
+    EditManagedRestaurant = 42,
+    DebugStorage = 404
 };
-// remember to update Admin Options when you update an option in ManagerOpt
-enum AdminOpt{
-    EditName,
-    // restaurant
-    AddRest,
-    Activate,
-    Deactivate,
-    
+
+enum class AdminAction : int {
+    Quit = 0,
+    CreateRestaurant = 1,
+    ActivateRestaurant = 2,
+    DeactivateRestaurant = 3,
+    CreateRestaurateur = 4,
+    PrintTotalSaleStatistics = 31,
+    PrintTotalCustomerStatistics = 32,
+    PrintRestaurantSaleStatistics = 33,
+    PrintRestaurantCustomerStatistics = 34,
+    DebugStorage = 404
 };
